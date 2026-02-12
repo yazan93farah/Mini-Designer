2ebc972ff3892a4b0d8fc624463f62228b8109bc
Yazan Farah <yazan93farah@gmail.com>
2026-02-12 02:42:15 +0100

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHeader({
+  title: "About this project",
+  showBack: true,
+  price: 15.45,
+});
+const router = useRouter();
+</script>
+
+<template>
+  <!-- Match layout space (100vh - header/footer 2×6rem) and center content -->
+  <div class="min-h-[calc(100vh-12rem)] flex items-center justify-center">
+    <div class="flex flex-col">
+      <div class="text-5xl text-gray-600 font-semibold">
+        This is a Demo Page
+      </div>
+      <BaseButton class="mt-4" color="blue" @click="router.back()"
+        >Back</BaseButton
+      >
+    </div>
+  </div>
+</template>
