87f1a6c1abd40fe846997e52f66aea4e8caba194
Yazan Farah <yazan93farah@gmail.com>
2026-02-12 02:41:30 +0100

    added button component

---
+++ components/BaseButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+type Color = "blue" | "green";
+
+interface Props {
+  color?: Color;
+  disabled?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: "blue",
+  disabled: false,
+});
+
+const variantClasses = computed(() => {
+  switch (props.color) {
+    case "green":
+      return "bg-buttonGreen text-black hover:bg-buttonGreen-dark";
+    case "blue":
+    default:
+      return "bg-buttonBlue text-white hover:bg-buttonBlue-dark";
+  }
+});
+</script>
+<template>
+  <button
+    :disabled="disabled"
+    :class="[
+      'align-center px-12 py-2.5 rounded-xl transition-colors duration-200 text-lg',
+      'focus:outline-none focus:ring-2 ',
+      variantClasses,
+      disabled && 'opacity-50 cursor-not-allowed',
+    ]"
+  >
+    <slot />
+  </button>
+</template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,22 @@
-import type {Config} from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
+import type { Config } from "tailwindcss";
+// import defaultTheme from "tailwindcss/defaultTheme";
 
 export default <Partial<Config>>{
-    content: [],
-    theme: {
-        extend: {},
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        buttonBlue: {
+          DEFAULT: "#007ff4",
+          dark: "#0069cc",
+        },
+        buttonGreen: {
+          DEFAULT: "#5df400",
+          dark: "#4cd000",
+        },
+        labelColor: "#0fce5b",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
